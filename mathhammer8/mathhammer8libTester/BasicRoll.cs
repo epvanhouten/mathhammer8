2970b1d684efed1b6924af1689467a4467e21c69
Erick Van Houten <epvanhouten@gmail.com>
2017-05-13 12:18:49 -0500

    Adding unit test project.

---
+++ mathhammer8/mathhammer8libTester/BasicRoll.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mathhammer8libTester
+{
+    [TestClass]
+    public class BasicRollTester
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
